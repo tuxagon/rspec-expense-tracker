1b1d8de5df5ae537b8b9c9f9476fad1962e5387b
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-15 15:29:13 -0400

    Added more logic to routes to record an expense

--- app/api.rb
+++ app/api.rb
@@ -1,14 +1,28 @@
 require 'sinatra/base'
 require 'json'
+require_relative 'ledger'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     get '/expenses/:date' do
       JSON.generate([])
     end
 
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
   end
 end
